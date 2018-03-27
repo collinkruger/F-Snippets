d1b0551ef327ca863e738c5cf005c4469cd9fb51
Collin Kruger <collin.kruger@live.com>
2018-03-27 18:57:13 -0500

    Add FileSystemKeyBytesPairStore

---
+++ FSharp-Snippets/Store/FileSystemKeyBytesPairStore.fsx
@@ -0,0 +1,28 @@
+﻿open System
+open System.IO
+
+module FileSystemKeyBytesPairStoreModule =
+
+    let private path rootDirectory key = Path.Combine(rootDirectory, key)
+
+
+    let set rootDirectory key bytes = File.WriteAllBytes(path rootDirectory key, bytes)
+
+    let get rootDirectory key =
+        try
+            Some (File.ReadAllBytes(path rootDirectory key))
+        with
+        | :? FileNotFoundException -> None
+
+    let remove rootDirectory key = File.Delete(path rootDirectory key)
+
+    let clear rootDirectory =
+        let directoryInfo = DirectoryInfo(rootDirectory)
+        directoryInfo.EnumerateDirectories() |> Seq.iter (fun di -> di.Delete(true))
+        directoryInfo.EnumerateFiles() |> Seq.iter (fun fi -> fi.Delete())
+
+type FileSystemKeyBytesPairStore(rootDirectory) =
+    member this.set key bytes = FileSystemKeyBytesPairStoreModule.set rootDirectory key bytes
+    member this.get key       = FileSystemKeyBytesPairStoreModule.get rootDirectory key
+    member this.remove key    = FileSystemKeyBytesPairStoreModule.remove rootDirectory key
+    member this.clear ()      = FileSystemKeyBytesPairStoreModule.clear rootDirectory
